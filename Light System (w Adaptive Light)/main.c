e99e4908513fb1b33747f113999c13c91922dce6
aliamaim <ali.amaim@yahoo.com>
2019-08-05 21:25:58 +0200

    Update main.c

--- Light System (w Adaptive Light)/main.c
+++ Light System (w Adaptive Light)/main.c
@@ -34,15 +34,20 @@ int main(void)
 	DDRD &= ~(1 << LEFTSIGNAL_IN | 1 << RIGHTSIGNAL_IN | 1 << WAITINGSIGNAL_IN);  // Set them as input
 	DDRD |= (1 << LEFTSIGNAL_OUT | 1 << RIGHTSIGNAL_OUT); // Set them as output
 	DDRB |= (1 << PORTB3);
-	PORTB |= (1 << PORTB3);
+	PORTB &= ~(1 << PORTB3);
+	DDRB &= ~(1 << PORTB4);
 	ICUInit();
 	Timer0OVFInit();
 	sei();
     /* Replace with your application code */
     while (1) 
     {
+		if(BIT_IS_SET(PORTD, PORTB4))
+		{
+			PORTB |= (1 << PORTB3);
+		}	
 		///////////////////// Turn Signals & Waiting ////////////////////////////////
-		if(BIT_IS_SET(PORTD, WAITINGSIGNAL_IN))
+		/*if(BIT_IS_SET(PORTD, WAITINGSIGNAL_IN))
 		{
 			if((BIT_IS_SET(PORTD, LEFTSIGNAL_OUT) && BIT_IS_SET(PORTD, RIGHTSIGNAL_OUT)) || 
 			   (BIT_IS_CLEAR(PORTD, LEFTSIGNAL_OUT) && BIT_IS_CLEAR(PORTD, RIGHTSIGNAL_OUT)) )
@@ -65,7 +70,7 @@ int main(void)
 		else
 		{
 			signal_state = Forward;
-		}
+		}*/
 		///////////////////////////////////////////////////////////////////////////
 		
 		
@@ -116,23 +121,50 @@ ISR(TIMER0_OVF_vect)
 	}
 	else if(signal_state == Left)
 	{
+		PORTB |= (1 << PORTB3);
 		TOGGLE_BIT(PORTD, LEFTSIGNAL_OUT);
 	}
 	else if(signal_state == Right)
 	{
+		//PORTB |= (1 << PORTB3);
 		TOGGLE_BIT(PORTD, RIGHTSIGNAL_OUT);
 	}
 	else
 	{
-		PORTD &= ~(1 << LEFTSIGNAL_OUT | 1 << RIGHTSIGNAL_OUT); // Turn off the signals
-		TIMSK0 &= ~(1 << TOIE0); // Disable timer interrupt so the ISR that toggles the turn signals doesn't get called
-		TCNT0 = 0; // Clear timer counter
+		//PORTB |= (1 << PORTB3);
+		//PORTD &= ~(1 << LEFTSIGNAL_OUT | 1 << RIGHTSIGNAL_OUT); // Turn off the signals
+		//TIMSK0 &= ~(1 << TOIE0); // Disable timer interrupt so the ISR that toggles the turn signals doesn't get called
+		//TCNT0 = 0; // Clear timer counter
 	}
 	
 }
 
 ISR(TIMER1_CAPT_vect)
 {
+	if(BIT_IS_SET(PORTD, WAITINGSIGNAL_IN))
+	{
+		if((BIT_IS_SET(PORTD, LEFTSIGNAL_OUT) && BIT_IS_SET(PORTD, RIGHTSIGNAL_OUT)) ||
+		(BIT_IS_CLEAR(PORTD, LEFTSIGNAL_OUT) && BIT_IS_CLEAR(PORTD, RIGHTSIGNAL_OUT)) )
+		{
+			PORTD &= ~(1 << LEFTSIGNAL_OUT | 1 << RIGHTSIGNAL_OUT); //Make sure both are the same state to avoid desync between the right & left signals.
+		}
+		_delay_ms(25); // Debounce time
+		if(BIT_IS_SET(PORTD, WAITINGSIGNAL_IN)) { signal_state = Waiting; }
+	}
+	else if(BIT_IS_SET(PORTD, LEFTSIGNAL_IN))
+	{
+		_delay_ms(25); // Debounce time
+		if(BIT_IS_SET(PORTD, LEFTSIGNAL_IN)) { signal_state = Left; }
+	}
+	else if(BIT_IS_SET(PORTD, RIGHTSIGNAL_IN))
+	{
+		_delay_ms(25); // Debounce time
+		if(BIT_IS_SET(PORTD, RIGHTSIGNAL_IN)) { signal_state = Right; }
+	}
+	else
+	{
+		signal_state = Forward;
+	}
 	// Enable the timer0 OVF interrupt which would control the signals.
 	TIMSK0 |= (1 << TOIE0);
 	// Call the timer0 which controls the turning signals for the first time so the driver would see immediate change (not neccessary)
